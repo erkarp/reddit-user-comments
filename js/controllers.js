505342e25c35d99db92bd2e74438fe39e7da8772
Emily <emilyrkarp@gmail.com>
2014-09-20 12:40:20 -0400

    moved js into folder, got username input field working, added ng-show to respin button. (bug: chart gets screwy if page isn't refreshed b/t username change)

---
+++ js/controllers.js
@@ -0,0 +1,28 @@
+angular.module('controllers',[])
+.controller('CloudController', ['$scope','Comments','PrepData','DrawChart', function($scope, Comments, PrepData, DrawChart){
+	
+	$scope.createChart = function(username) {
+		console.log(username);
+		Comments.get(username)
+		.then(function(array) {
+			$scope.comments = array.data.data.children;
+			return PrepData.parse(array.data.data.children);
+		})
+		.then(function(result) {
+			$scope.setData(result);
+			$scope.colorChart();
+		});
+	};
+	$scope.setData = function(data) {
+		$scope.data = data;
+		$scope.subreddits = $scope.data.datasets;
+	};
+	$scope.colorChart = function() {
+		$scope.myChart = { "data": DrawChart.make($scope.data), "options": { bezierCurve:false } };
+	};
+}]);
+//http://stackoverflow.com/questions/13937318/convert-angular-http-get-function-to-a-service
+		/* make big string
+		array.data.data.children.reduce(function(prev, comment){
+				return prev += comment.data.body;
+			}, '');	*/
