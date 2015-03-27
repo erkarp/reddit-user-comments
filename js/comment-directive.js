5f3b8d37f9f9690ddd63f5b25075181622276333
Emily <emilyrkarp@gmail.com>
2015-03-27 13:37:57 -0400

    comment-directive template to templateUrl; ++ comment.html

--- js/comment-directive.js
+++ js/comment-directive.js
@@ -1,7 +1,7 @@
 app.directive('comment', function () {
     return {
       restrict: 'A',
-	  template: '<p><span style="background-color:{{commentData.color}}" class="subreddit"><a href="http://www.reddit.com/r/{{commentData.subreddit}}">{{commentData.subreddit}}:</a></span><a href="{{commentData.link_url}}">{{commentData.link_title}}</a><span class="date">{{commentData.posted}}</span></p><p>{{commentData.body}} <a class="perma" href="{{commentData.link_url+commentData.id}}">[link]</a></p>',
+	  templateUrl: 'comment.html',
       scope: {
         commentData: '=info'
       },
