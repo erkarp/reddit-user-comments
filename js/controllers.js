053f4ecbd612cc18da5ef25dff70db10663b3ec7
Emily <emilyrkarp@gmail.com>
2015-03-27 08:49:57 -0400

    ng-show...errorClass; boo when errorClass is false in ctrl

--- js/controllers.js
+++ js/controllers.js
@@ -9,7 +9,7 @@ app.controller('CloudController', ['$scope','$http','Comments','PrepData','DrawC
 		.then(function(array) {
 			$scope.comments = Comments.dates(array.data.data.children);
 			if ($scope.comments[11] !== undefined) {
-				$scope.errorClass = '';
+				$scope.errorClass = false;
 				return PrepData.parse($scope.comments);
 			} else { 
 				$scope.errorClass = 'orange';
