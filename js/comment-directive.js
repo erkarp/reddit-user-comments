b83823df45f096cabdcb84a6abb0a860a05c7480
Emily <emilyrkarp@gmail.com>
2015-09-04 19:30:59 -0400

    moved element.css change into loop+iff

--- js/comment-directive.js
+++ js/comment-directive.js
@@ -1,1 +1,23 @@
-﻿app.directive('comment', function () {    return {      restrict: 'A',	  templateUrl: 'comment.html',      scope: {        colorData: '=color',        commentData: '=info'      },      link: function (scope, element, attrs) {          var color = '';                 for (var i = 0; i<scope.colorData.length; i++) {            if (scope.colorData[i].label == scope.commentData.subreddit) {                color = scope.colorData[i].pointColor;                break;            }        }            element.css({            backgroundColor: color        });          }    };  });
+app.directive('comment', function () {
+    return {
+      restrict: 'A',
+	  templateUrl: 'comment.html',
+      scope: {
+        colorData: '=color',
+        commentData: '=info'
+      },
+      link: function (scope, element, attrs) {
+		var color = ''; 
+
+		for (var i = 0; i<scope.colorData.length; i++) {
+			if (scope.colorData[i].label == scope.commentData.subreddit) {
+				element.css({
+					backgroundColor:  scope.colorData[i].pointColor
+				}); 
+				break;
+			}
+		}
+    
+      }
+    };
+  });
