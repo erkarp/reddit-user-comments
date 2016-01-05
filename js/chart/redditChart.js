f5ee3b1111a2d01644045c98f5a8b959ecced474
Emily <emilyrkarp@gmail.com>
2016-01-04 23:54:19 -0500

    removed ChartJS completely

--- js/chart/redditChart.js
+++ js/chart/redditChart.js
@@ -1,85 +1,61 @@
-var app = angular.module('MyApp', ['chartjs-directive']);
+var app = angular.module('MyApp', []);
 
 app.factory('PrepData', [function() {
 	return {
 		parse: function(comments) {
-			var data = { 
-				labels: [], 
-				datasets: [] 
+			var data = {
+				labels: [],
+				datasets: []
 			};
-			
+
 			for (var i=0; i<comments.length; i++) {
-				
+
 				var monthYear = this.monthYr(comments[i].data.created);
 				if (i > 0) {
 					data = this.addLine(data, comments[i].data.subreddit, false);
 				}
-				
+
 				data = this.addDate(data, monthYear);
 				var temp = this.getLine(data, comments[i].data.subreddit);
 				temp[data.labels.indexOf(monthYear)]++;
 			}
 			return data;
-		},	
-		
+		},
+
 		monthYr: function(created) {
 			var timestamp = new Date(created * 1000);
 			return timestamp.getMonth() + " " + timestamp.getFullYear();
 		},
-		
+
 		addLine: function(data, subreddit) {
 			if (this.getLine(data, subreddit, true) == -1) {
-				data.datasets.push({ 
-					label: subreddit, 
-					data: [0], 
+				data.datasets.push({
+					label: subreddit,
+					data: [0],
 					total: 0
 				});
 			} return data;
 		},
-		
+
 		addDate: function(data, monthYear) {
 			if (data.labels.indexOf(monthYear) == -1) {
 				data.labels.push(monthYear);
-				
+
 				data.datasets.forEach(function(item){
 					item.data.push(0);
 				});
 			} return data;
 		},
-		
-		getLine: function(data, subreddit, fromAddLine) { 
+
+		getLine: function(data, subreddit, fromAddLine) {
 			for (var i=0; i<data.datasets.length; i++) {
 				if (data.datasets[i].label == subreddit) {
-					if (! fromAddLine) { 
-						data.datasets[i].total++; 
+					if (! fromAddLine) {
+						data.datasets[i].total++;
 					}
 					return data.datasets[i].data;
 				}
 			} return -1;
-		}
-	};
-}])
-.factory('DrawChart', [function() {
-	return {
-		make: function(data){
-			for (var i=0; i<data.datasets.length; i++) {
-				var col = randomColor({ 
-					luminosity: 'bright', 
-					format: 'rgb' 
-				});
-				data.datasets[i] = this.color(data.datasets[i], col);
-			}	
-			return data;
-		},
-		
-		color: function(dataset, col) {
-			dataset.fillColor = "rgba(0,0,0,0)";
-			dataset.strokeColor = col;
-			dataset.pointColor = col;
-			dataset.pointStrokeColor = "#fff";
-			dataset.pointHighlightFill = "#fff";
-			dataset.pointHighlightStroke = col;
-			return dataset;
 		}
 	};
 }]);
